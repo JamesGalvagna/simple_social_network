d969899a927ce3b11e82a04033ad856b56478350
James Galvagna <jgalvagn@umd.edu>
2022-03-13 15:02:41 -0400

    add social net py file to local

---
+++ simple_social_net.py
@@ -0,0 +1,10 @@
+'''social network for tracking connections between people'''
+
+class Person:
+    ''' A person in the social network.
+    
+    Attributes:
+        name (str): person's name
+        connections (set of Person): other people in the social network
+            who know this person 
+    '''
